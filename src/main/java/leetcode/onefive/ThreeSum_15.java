e35b728c28e6150c0237547269efe50d77f7275f
LeeJeKyun <leejk@metabuild.co.kr>
2025-01-14 18:08:21 +0900

    배열 3번(leetcode-3) 투포인터활용

--- src/main/java/leetcode/onefive/ThreeSum_15.java
+++ src/main/java/leetcode/onefive/ThreeSum_15.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class ThreeSum_15 {
 }
 
-class Solution_mine {
+class Solution_twopointer {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
 
@@ -15,15 +15,20 @@ class Solution_mine {
         for(int i=0; i<nums.length; i++) {
             if(i > 0 && nums[i] == nums[i-1])
                 continue;
-            for(int j=i+1; j<nums.length; j++) {
-                for(int k=j+1; k<nums.length; k++) {
-                    if(nums[i] + nums[j] + nums[k] == 0) {
-                        List<Integer> set = new ArrayList<>();
-                        set.add(nums[i]);
-                        set.add(nums[j]);
-                        set.add(nums[k]);
-                        result.add(set);
-                    }
+            int left = i+1;
+            int right = nums.length - 1;
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
