4fb3b62f021f061ae7b69d10f01924f7cf798d30
LeeJeKyun <leejk@metabuild.co.kr>
2025-01-21 09:49:46 +0900

    배열 5번(leetcode-121) 풀이 완료

---
+++ src/main/java/leetcode/onetwoone/BestTimeToBuyAndSellStock_121.java
@@ -0,0 +1,25 @@
+package leetcode.onetwoone;
+
+public class BestTimeToBuyAndSellStock_121 {
+
+    public static void main(String[] args) {
+        int[] input = new int[]{8,1,5,3,6,4};
+        Solution solution = new Solution();
+        int answer = solution.maxProfit(input);
+        System.out.println("answer = " + answer);
+    }
+
+}
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int min = prices[0];
+        for(int price : prices) {
+            min = Math.min(min, price);
+            max = Math.max(max, price-min);
+        }
+
+        return max;
+    }
+}
